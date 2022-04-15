9686e925ae357666e2f90afa818e9c6936111203
layetri <daniel@mainstreamct.com>
2022-04-15 13:00:09 +0200

    Updated synthobject

--- src/synthobjects/SynthObject.js
+++ src/synthobjects/SynthObject.js
@@ -5,19 +5,28 @@ export default class SynthObject {
         this.parameters = [];
         this.type = type;
         this.voice_group = 0;
-
-        if(type === "modulator") {
-            this.initpos = {x: Math.random() * (window.innerWidth - 200) / 2, y: 50 + (Math.random() * (window.innerHeight - 250))}
-        } else {
-            this.initpos = {x: ((Math.random() * (window.innerWidth - 200) / 2) + window.innerWidth / 2) - 100, y: 50 + (Math.random() * (window.innerHeight - 250))}
-        }
+        this.initpos = {x: 0, y: 0}
         
         for(let i = 0; i < parameters.length; i++) {
-            this.parameters.push({name: name + "_" + parameters[i], value: 0.00, position: {x: this.initpos.x, y: (this.initpos.y + 40) + i * 20}, links: 0});
+            this.parameters.push({name: name + "_" + parameters[i], value: 0.00, position: {x: 0, y: 0}, links: 0});
         }
 
         this.links = 0;
-        this.position = {x: this.initpos.x + 150, y: this.initpos.y + 70}
+        this.position = {x: 0, y: 0}
+        this.randomPos();
+    }
+
+    randomPos() {
+        if (this.type === "modulator") {
+            this.initpos = { x: Math.random() * (window.innerWidth - 200) / 2, y: 50 + (Math.random() * (window.innerHeight - 250)) }
+        } else {
+            this.initpos = { x: ((Math.random() * (window.innerWidth - 200) / 2) + window.innerWidth / 2) - 100, y: 50 + (Math.random() * (window.innerHeight - 250)) }
+        }
+
+        this.position = { x: this.initpos.x + 150, y: this.initpos.y + 70 };
+        for (let i = 0; i < this.parameters.length; i++) {
+            this.parameters[i].position = { x: this.initpos.x, y: (this.initpos.y + 40) + i * 20 };
+        }
     }
 
     setVoiceGroup(group) {
@@ -25,7 +34,6 @@ export default class SynthObject {
     }
 
     setPosition(x, y, name) {
-        console.log(name);
         if(name === "output") {
             this.position = {x: x, y: y};
         } else {
