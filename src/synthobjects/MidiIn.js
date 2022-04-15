40817c26198c9a580e06b5ccd673f31d555bae6d
layetri <daniel@mainstreamct.com>
2022-04-15 12:59:12 +0200

    Added midi input object

--- src/synthobjects/MidiIn.js
+++ src/synthobjects/MidiIn.js
@@ -1,8 +1,11 @@
 import SynthObject from "./SynthObject";
 
 export default class MidiIn extends SynthObject {
-    constructor(cc, channel) {
-        super("MIDI CC " + cc, "midi_" + cc, "midi", []);
+    constructor(cc, channel, name = false) {
+        if(!name) {
+            name = "midi_" + cc;
+        }
+        super("MIDI CC " + cc, name, "midi", []);
 
         this.cc = cc;
         this.channel = channel;
